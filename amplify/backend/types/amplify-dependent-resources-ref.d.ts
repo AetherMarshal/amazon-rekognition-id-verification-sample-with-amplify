daa6f568ae6444ef6041a5c5ce8ffb404614ddf0
SecureSpace <116037977+AetherMarshal@users.noreply.github.com>
2023-03-04 18:50:01 +0530

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,51 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amazonrekognitionidv": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    },
+    "test123": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "amazonrekognitionidv6570cbea": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "adminGroupRole": "string",
+      "poweruserGroupRole": "string",
+      "viewerGroupRole": "string"
+    }
+  },
+  "function": {
+    "S3Triggerb6cacc90": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "idvworkflowfn": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "idvimagebucket": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
